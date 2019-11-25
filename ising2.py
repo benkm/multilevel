ec638eda0371021d07da256317cff823abbfbda1
Ben Kitching-Morley <bkm1n18@soton.ac.uk>
2019-11-25 13:27:07 +0000

    Swapping over to PC

--- ising2.py
+++ ising2.py
@@ -8,6 +8,97 @@ from tqdm import tqdm
 from density_calculator import *
 
 
+def region_maker(L, s):
+  """
+    This function will return a function, which can be used to see which region
+    of the lattice a given point is in.
+
+    L : int, size of the lattice
+    s : (3, ) tuple of ints, describing how the lattice is to be broken
+      down in the directions (x1, ..., x3). For simlicity I have programmed this
+      to only accept ints that divide L exactly e.g. powers of 2
+  """
+  dim = len(s)
+
+  shape = (L, ) * dim
+  regions = numpy.zeros(shape)
+
+  for i in s:
+    assert type(i) == int
+    assert i >= 1
+    assert L % i == 0
+
+  # Size of sublattices in each direction
+  size = L // numpy.array(s)
+
+  # Use a recursive function to label the regions uniquely
+  def label(x, n):
+    if n == 0:
+      return 0
+
+    # Add 1 to distiguish regions from the boundary
+    if n == dim:
+      return label(x, n - 1) * s[n - 1] + x[n - 1] // size[n - 1] + 1
+
+    else:
+      return label(x, n - 1) * s[n - 1] + x[n - 1] // size[n - 1]
+
+  # If there is no s then there is only one region with no boudary
+  if numpy.product(numpy.array(s)) == 1:
+    def which_region(x, x2=None, x3=None):
+      return 1
+
+  else:
+    def which_region(x):
+      """
+        This function returns the region of the lattice a coordinate is in
+
+        x : (d, ) list of ints, representing the coordinate on the lattice. Can
+          also be a single int, in the case that the other coordinates are included
+
+        RETURNS :
+        ---------
+        region : int, label of region. Note that region 0 is reserved for the boundary.
+      """
+      for i in x:
+        assert type(i) == int
+        assert i < L and i >= 0
+      
+      # First check if the coordinate is on the boundary. If so return 0.
+      for i in range(dim):
+        if s[i] > 1:
+          if x[i] % (L // s[i]) == 0:
+             return 0
+
+      # If the point isn't on the boundary then use the recursive function
+      # label defined previously
+      return label(x, dim)
+  
+  # Now use the function which_region to caclulate the region of each point
+  regions = numpy.zeros(shape)
+
+  indices = numpy.indices(shape)
+
+  def cycle(regions, stored, dim):
+    if dim == 1:
+      for i in range(L):
+        new_stored = stored + [i]
+        regions[i] = which_region(new_stored)
+      
+      return regions
+
+    else:
+      for i in range(L):
+        new_stored = stored + [i]
+        regions[i] = cycle(regions[i], new_stored, dim - 1)
+      
+      return regions
+
+  regions = cycle(regions, [], dim)
+
+  return regions
+
+
 def plot_spins(spins, ax):
   L = spins.shape[0]
 
@@ -34,22 +125,6 @@ def chessboard(L):
     for j in range(L):
       board[i, j] = (i + j) % 2
   return board
-
-
-def non_boundary(L, splitting):
-  grid = numpy.ones((L, L))
-
-  if splitting[0] != 1:
-    step = L // splitting[0]
-    for i in range(splitting[0]):
-      grid[i * step, :] = 0
-
-  if splitting[1] != 1:
-    step = L // splitting[1]
-    for i in range(splitting[1]):
-      grid[:, i * step] = 0
-
-  return grid
 
 
 def neighbour_total(spin_array):
@@ -89,9 +164,8 @@ class lattice(object):
 
     # For reduced memory usage
     self.spins = numpy.array(spins, dtype=numpy.int8)
-
     self.chessboard = chessboard(L)
-    self.non_boundary = non_boundary(L, splitting)
+    self.regions = region_maker(L, splitting)
     self.beta = beta
 
     # To be clear this is the total energy associated with all interactions at
@@ -113,8 +187,9 @@ class lattice(object):
     delta_E = - self.energies * 2
     jump_condition = numpy.exp(- self.beta * delta_E) > numpy.random.rand(self.L, self.L)
 
+    # The boundary has self.regions = 0, all other locations are non-boundary
     to_flip = numpy.logical_and(jump_condition,
-                    numpy.logical_and(self.non_boundary, self.chessboard))
+                    numpy.logical_and(self.regions, self.chessboard))
 
     self.spins = numpy.where(to_flip, -self.spins, self.spins)
     self.energies = - self.spins * neighbour_total(self.spins)
@@ -124,7 +199,7 @@ class lattice(object):
     jump_condition = numpy.exp(- self.beta * delta_E) > numpy.random.rand(self.L, self.L)
 
     to_flip = numpy.logical_and(jump_condition,
-                    numpy.logical_and(self.non_boundary, numpy.logical_not(self.chessboard)))
+                    numpy.logical_and(self.regions, numpy.logical_not(self.chessboard)))
 
     self.spins = numpy.where(to_flip, -self.spins, self.spins)
     self.energies = - self.spins * neighbour_total(self.spins)
@@ -214,107 +289,176 @@ def generate_states_splitting(L, beta, N, M, step, splitting, source_file_N, sou
     run()
 
 
-def twopt_combos(spins, regions, weights, delta):
+def normalize_weights(weights, regions):
+  """
+    This function normalizes weights (type = 1), so that the average weight in
+    the boundary region is 1
+
+    weights : (L, L) floats
+    regions : (L, L) ints where entries of 0 are at the boundary
+  """
+  # Find the total size of the boundary
+  size = numpy.sum(regions == 0)
+
+  # Find the total of the weights in this region
+  wegiht_total = numpy.sum(weights * (regions == 0))
+
+  # Therefore the average weight is given by weight_total/sum
+  av = wegiht_total / size
+
+  # Rescale the weights so that the boundary is 1
+  weights = weights / av
+
+  return weights
+
+
+def twopt_average(spins, regions, weights, delta, weight_type=1):
   """
     Here we are finding the real space twopt function of many spin configurations,
     which are assumed to share the same boundary.
 
-    spins : (N, M, L, L) numpy array of floats, spins[i] is the ith spin configuration
+    spins : (N, M, L, ..., L) numpy array of floats, spins[i] is the ith spin configuration
         from a group of spins holding
-    regions : (L, L) numpy array of ints, which label the regions of the lattice
+    regions : (L, ..., L) numpy array of ints, which label the regions of the lattice
         according to the splitting
-    delta : (2, ) tuple, list or array of ints in [0, L). This represents the 
+    delta : (d, ) tuple, list or array of ints in [0, L). This represents the 
         separation vector between two points
-    weights : (L, L) array of floats
+    weights : (L, ..., L) array of floats. These should be normalized so that the
+      weights on the boundary are 1, or average to 1.
+    weight_type : int, if 1 then the weights are 'onept' site by site weights.
+      If 2 then these are the twopt weights, ready to be used directly.
 
     N is the number of boundary updates
     where M is the number of sublattice samples
     L is the size of the lattice
+    d is the number of dimensions
   """
   N = spins.shape[0]
   M = spins.shape[1]
   L = regions.shape[0]
+  d = len(regions.shape)
   assert spins.shape[2] == L
+  assert len(spins.shape) == d + 2
 
-  estimators = numpy.zeros((L, L))
+  estimators = numpy.zeros((L, ) * d)
+
+  # First normalize the weights
+  weights = normalize_weights(weights, regions)
 
   # Bring the points displaced by delta to the point x
-  yspins = numpy.roll(numpy.roll(spins, -delta[0], axis=2), -delta[1], axis=3)
+  yspins = spins
+  yregions = regions
+  yweights = weights
+  for i in range(d):
+    yspins = numpy.roll(yspins, -delta[i], axis=i + 2)
 
-  # Do the same for the regions array
-  yregions = numpy.roll(numpy.roll(regions, -delta[0], axis=0), -delta[1], axis=1)
-
-  yweights = numpy.roll(numpy.roll(weights, -delta[0], axis=0), -delta[1], axis=1)
+    # Do the same with regions and weights
+    yregions = numpy.roll(yregions, -delta[i], axis=i)
+    yweights = numpy.roll(yweights, -delta[i], axis=i)
   
   # Perform an outer product over the configuration axis
-  # This produces an array of shape (N, M, M, L, L)
-  outer_product = numpy.einsum('ij...,ik...->ijk...', spins, yspins)
+  # This produces an array of shape (N, M, M, L, ..., L)
+  combinatoric = numpy.einsum('ij...,ik...->ijk...', spins, yspins)
+
+  # BE CAREFUL here! We've made combinatoric states. However, this isn't physically
+  # valid for twopt functions between points in the same region. This isn't valid
+  # within region. It's okay on the boundary because these states are automatically
+  # constant along the M-axis.
+  within_region = (spins * yspins).reshape((N, M, 1) + spins.shape[2:]).repeat(M, axis=2)
+
+  outer_product = numpy.where(regions != yregions, combinatoric, within_region)
 
   # Average over the 2 outer product directions and over the N axis
   config_mean = numpy.mean(outer_product, axis=(0, 1, 2))
 
-  # Turn the site by site weights into 2 point correlator weights
+  if weight_type == 1:
+    ## Turn the site by site weights into 2 point correlator weights
 
-  # Only in the case of the 
-  weights = numpy.where(numpy.logical_and(regions * yregions != 0, regions != yregions, )
+    # Only in the case of two different non-boundary regions do we product the weights together.
+    weights = numpy.where(numpy.logical_and(regions * yregions != 0, regions != yregions),
+                          weights * yweights,
+                          numpy.maximum(weights, yweights))
+
+  # Perform a weighted average over the spatial dimensions
+  estimator = numpy.average(config_mean, weights=weights)
+
+  return estimator
 
 
-def twopt_combo_bootstrap(spins, regions, delta, boot_samples, return_weights=False):
-  """
-    Here we are finding the real space twopt function of many spin configurations,
-    which are assumed to share the same boundary.
+def defunct():
+  return 0
+  # def twopt_combo_bootstrap(spins, regions, delta, boot_samples, return_weights=False):
+  #   """
+  #     Here we are finding the real space twopt function of many spin configurations,
+  #     which are assumed to share the same boundary.
 
-    spins : (N, M, L, L) numpy array of floats, spins[i] is the ith spin configuration
-        from a group of spins holding
-    regions : (L, L) numpy array of ints, which label the regions of the lattice
-        according to the splitting
-    delta : (2, ) tuple, list or array of ints in [0, L). This represents the 
-        separation vector between two points
-    boot_samples : number fo bootstrap samples used.
-    return_weights : Bool, if true then the weights are also returned, e.g. for
-      plotting etc.
+  #     spins : (N, M, L, L) numpy array of floats, spins[i] is the ith spin configuration
+  #         from a group of spins holding
+  #     regions : (L, L) numpy array of ints, which label the regions of the lattice
+  #         according to the splitting
+  #     delta : (2, ) tuple, list or array of ints in [0, L). This represents the 
+  #         separation vector between two points
+  #     boot_samples : number fo bootstrap samples used.
+  #     return_weights : Bool, if true then the weights are also returned, e.g. for
+  #       plotting etc.
 
-    N is the number of boundary updates
-    where M is the number of sublattice samples
-    L is the size of the lattice
-  """
-  N = spins.shape[0]
-  M = spins.shape[1]
-  L = regions.shape[0]
-  assert spins.shape[2] == L
+  #     N is the number of boundary updates
+  #     where M is the number of sublattice samples
+  #     L is the size of the lattice
+  #   """
+  #   N = spins.shape[0]
+  #   M = spins.shape[1]
+  #   L = regions.shape[0]
+  #   assert spins.shape[2] == L
 
-  estimators = numpy.zeros((L, L))
+  #   estimators = numpy.zeros((L, L))
 
-  # Bring the points displaced by delta to the point x
-  yspins = numpy.roll(numpy.roll(spins, -delta[0], axis=2), -delta[1], axis=3)
+  #   # Bring the points displaced by delta to the point x
+  #   yspins = numpy.roll(numpy.roll(spins, -delta[0], axis=2), -delta[1], axis=3)
 
-  # Do the same for the regions array
-  yregions = numpy.roll(numpy.roll(regions, -delta[0], axis=0), -delta[1], axis=1)
+  #   # Do the same for the regions array
+  #   yregions = numpy.roll(numpy.roll(regions, -delta[0], axis=0), -delta[1], axis=1)
 
-  # Perform an outer product over the configuration axis
-  # This produces an array of shape (N, M, M, L, L)
-  outer_product = numpy.einsum('ij...,ik...->ijk...', spins, yspins)
+  #   # Perform an outer product over the configuration axis
+  #   # This produces an array of shape (N, M, M, L, L)
+  #   outer_product = numpy.einsum('ij...,ik...->ijk...', spins, yspins)
 
-  # Average over the M^2 sublattice configs only.
-  sub_average = numpy.mean(outer_product, axis=(1, 2))
+  #   # Average over the M^2 sublattice configs only.
+  #   sub_average = numpy.mean(outer_product, axis=(1, 2))
 
-  # Perform a bootstrap over the remaining N degrees of freedom 
-  random_indices = numpy.random.randint(0, N, size=(boot_samples, N))
+  #   # Perform a bootstrap over the remaining N degrees of freedom 
+  #   random_indices = numpy.random.randint(0, N, size=(boot_samples, N))
 
-  results = numpy.zeros((no_configs, L, L))
-  for sample in range(boot_samples):
-    data = sub_average[random_indices[sample]]
-    results[sample] = numpy.mean(data, axis=0)
-  
-  # We then get 2 (L, L) arrays, with bootstrap means and stds for each lattice
-  # site
-  means = numpy.mean(results, axis=0)
-  stds = numpy.std(results, axis=0)
+  #   results = numpy.zeros((no_configs, L, L))
+  #   for sample in range(boot_samples):
+  #     data = sub_average[random_indices[sample]]
+  #     results[sample] = numpy.mean(data, axis=0)
+    
+  #   # We then get 2 (L, L) arrays, with bootstrap means and stds for each lattice
+  #   # site
+  #   means = numpy.mean(results, axis=0)
+  #   stds = numpy.std(results, axis=0)
 
-  # Use the inverse varience weighting method
-  weights = 1 / (stds ** 2)
+  #   # Use the inverse varience weighting method
+  #   weights = 1 / (stds ** 2)
 
-  if return_weights:
-    return numpy.average(means, weights=weights), weights
-  
-  return numpy.average(means, weights=weights)
+  #   if return_weights:
+  #     return numpy.average(means, weights=weights), weights
+    
+  #   return numpy.average(means, weights=weights)
+
+
+  # def non_boundary(L, splitting):
+  #   grid = numpy.ones((L, L))
+
+  #   if splitting[0] != 1:
+  #     step = L // splitting[0]
+  #     for i in range(splitting[0]):
+  #       grid[i * step, :] = 0
+
+  #   if splitting[1] != 1:
+  #     step = L // splitting[1]
+  #     for i in range(splitting[1]):
+  #       grid[:, i * step] = 0
+
+  #   return grid
--- test_ising.py
+++ test_ising.py
@@ -1,6 +1,29 @@
 from ising2 import *
 
 
+def test_region_maker():
+  L = 8
+  # Simplest case
+  regions = region_maker(L, (1, ))
+
+  assert numpy.array_equal(regions, numpy.array([1] * L))
+
+  # Split the 1D lattice
+  regions = region_maker(L, (2, ))
+  assert numpy.array_equal(regions, numpy.array([0, 1, 1, 1, 0, 2, 2, 2]))
+
+  # Let's try a 2D situation
+  regions = region_maker(L, (2, 2))
+  assert numpy.array_equal(regions, numpy.array([[0, 0, 0, 0, 0, 0, 0, 0],
+                                                 [0, 1, 1, 1, 0, 2, 2, 2],
+                                                 [0, 1, 1, 1, 0, 2, 2, 2],
+                                                 [0, 1, 1, 1, 0, 2, 2, 2],
+                                                 [0, 0, 0, 0, 0, 0, 0, 0],
+                                                 [0, 3, 3, 3, 0, 4, 4, 4],
+                                                 [0, 3, 3, 3, 0, 4, 4, 4],
+                                                 [0, 3, 3, 3, 0, 4, 4, 4]]))
+
+
 def test_chessboard():
   my_chessboard = chessboard(4)
 
@@ -8,15 +31,6 @@ def test_chessboard():
                                                        [1, 0, 1, 0],
                                                        [0, 1, 0, 1],
                                                        [1, 0, 1, 0]]))
-
-
-def test_non_boundary():
-  my_non_boundary = non_boundary(4, splitting=(1, 2))
-
-  assert numpy.array_equal(my_non_boundary, numpy.array([[0, 1, 0, 1],
-                                                         [0, 1, 0, 1],
-                                                         [0, 1, 0, 1],
-                                                         [0, 1, 0, 1]]))
 
 
 def test_neighbour_total():
@@ -117,3 +131,150 @@ def test_generate_states_1_2():
 
   # Check other parts of the lattice are changed
   assert numpy.sum((sublattice_data[..., 3] - sublattice_data[:, 0, :, 3].reshape(N, 1, L).repeat(M, axis = 1)) ** 2) != 0
+
+
+def test_normalize_weights():
+  # Try if all weights are one
+  L = 4
+
+  weights = numpy.ones((L, L))
+  regions = region_maker(L, (2, 2))
+
+  norm_weights = normalize_weights(weights, regions)
+
+  assert numpy.array_equal(norm_weights, weights)
+
+  # Try if all the weights are a constant not equal to one
+  weights = numpy.ones((L, L)) * 2
+
+  norm_weights = normalize_weights(weights, regions)
+
+  assert numpy.array_equal(norm_weights, numpy.ones((L, L)))
+
+  # Test a more realistic situation where the weights on the boundary are different
+  weights = numpy.array([[2, 2, 2, 2],
+                         [2, 5, 2, 5],
+                         [2, 2, 2, 2],
+                         [2, 5, 2, 5]])
+
+  norm_weights = normalize_weights(weights, regions)
+
+  assert numpy.array_equal(norm_weights, weights / 2)
+
+  # Let's try a slightly more difficult case
+  weights = numpy.array([[1, 1, 1, 1],
+                         [1, 5, 1, 5],
+                         [2, 2, 2, 2],
+                         [2, 5, 2, 5]])
+
+  norm_weights = normalize_weights(weights, regions)
+
+  assert numpy.array_equal(norm_weights, weights / 1.5)
+
+
+def test_twopt_average():
+  L = 8
+  N = 5
+  M = 10
+  regions = region_maker(L, (2, 2))
+  spins = numpy.zeros((N, M, L, L))
+  for i in range(N):
+    for j in range(M):
+      spins[i, j] = regions
+
+  # First check the simple twopt weights
+  weights = numpy.ones((L, L))
+  delta = (0, 0)
+  estimator = twopt_average(spins, regions, weights, delta, weight_type=2)
+
+  # Compare to hand caculation
+  assert estimator == numpy.mean(regions ** 2)
+
+  # I think that for any way of rolling the array the result will be lower
+  for i in range(1, L):
+    for j in range(1, L):
+      delta = (i, j)
+
+      estimator_2 = twopt_average(spins, regions, weights, delta, weight_type=2)
+
+      assert estimator_2 < estimator
+  
+  # Check that for a constant spin matrix, the result comes out the same regardless
+  x = numpy.random.rand()
+  spins = numpy.ones((N, M, L, L)) * x
+
+  for i in range(L):
+    for j in range(L):
+      for weight_type in [1, 2]:
+        delta = (i, j)
+
+        weights = numpy.random.rand(L, L)
+        estimator = twopt_average(spins, regions, weights, delta, weight_type=weight_type)
+
+        assert numpy.abs(estimator - x ** 2) < 10 ** -10
+  
+  # Let's check that given knowledge of the error by lattice site, we can achieve
+  # a reduced error through use of weights
+  weights = numpy.random.rand(L, L)
+
+  weights_extended = weights.reshape((1, 1, L, L)).repeat(N, axis=0).repeat(M, axis=1)
+
+  spins = numpy.ones((N, M, L, L)) + numpy.random.rand(N, M, L, L) * 0.1 * weights
+
+  # Make sure the boundaries are constant in the N direction
+  regions_extended = regions.reshape(1, 1, L, L).repeat(N, axis=0).repeat(M, axis=1)
+  spins = numpy.where(regions, spins, spins[:, 0, ...].reshape(N, 1, L, L).repeat(M, axis=1))
+
+  # Let's check that the boundarys indeed have a larger standard dev
+  # Perform an average in the M axis
+  boot_spins = numpy.mean(spins, axis=1)
+  random = numpy.random.randint(0, N, size=(100, N))
+  results = numpy.zeros((100, L, L))
+
+  for i in range(100):
+    some_spins = boot_spins[random[i]]
+    results[i] = numpy.mean(some_spins, axis=0)
+
+  means = numpy.mean(results, axis=0)
+  stds = numpy.std(results, axis=0)
+
+  boundary = regions == 0
+  non_boundary = regions != 0
+
+  boundary_av_std = numpy.sum(boundary * stds) / numpy.sum(boundary)
+  non_boundary_av_std = numpy.sum(non_boundary * stds) / numpy.sum(non_boundary)
+
+  # In theory the boundary std should be sqrt(M) larger
+  assert boundary_av_std > non_boundary_av_std * numpy.sqrt(M) * 0.7
+
+  # Now let's use the weights, and perform a bootstrap
+  # First try with no delta
+  estimator_1 = numpy.zeros(100)
+  estimator_2 = numpy.zeros(100)
+
+  for i in range(100):
+    # naiive method
+    estimator_1[i] = twopt_average(spins[random[i]], regions, numpy.ones((L, L)), (0, 0), weight_type=2)
+
+    # using weights, with inverse-varience method
+    estimator_2[i] = twopt_average(spins[random[i]], regions, 1 / weights ** 2, delta, weight_type=1)
+  
+  mean_1 = numpy.mean(estimator_1)
+  mean_2 = numpy.mean(estimator_2)
+  
+  std_1 = numpy.std(estimator_1)
+  std_2 = numpy.std(estimator_2)
+
+  pdb.set_trace()
+
+  assert std_2 < std_1
+
+
+
+# def test_non_boundary():
+#   my_non_boundary = non_boundary(4, splitting=(1, 2))
+
+#   assert numpy.array_equal(my_non_boundary, numpy.array([[0, 1, 0, 1],
+#                                                          [0, 1, 0, 1],
+#                                                          [0, 1, 0, 1],
+#                                                          [0, 1, 0, 1]]))
